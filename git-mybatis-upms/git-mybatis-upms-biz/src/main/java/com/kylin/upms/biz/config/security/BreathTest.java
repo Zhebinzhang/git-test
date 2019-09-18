5befe0d7ce8bb86f6d6a588e6534c0454f257d80
zhangzhebin <19913143147@163.com>
2019-09-18 14:43:30 +0800

    test-branch 测试test分支的方法

---
+++ git-mybatis-upms/git-mybatis-upms-biz/src/main/java/com/kylin/upms/biz/config/security/BreathTest.java
@@ -0,0 +1,11 @@
+package com.kylin.upms.biz.config.security;
+
+/**
+ * @Author: zhebin
+ * @Date: 2019/9/18 14:42
+ */
+public class BreathTest {
+    public static void main(String[] args) {
+        System.out.println("这是测试test-branch分支的方法");
+    }
+}
