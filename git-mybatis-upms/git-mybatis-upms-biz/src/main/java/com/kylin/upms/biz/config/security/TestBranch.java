bb589c37f2409795318be587920a4f42c5f13136
zhangzhebin <19913143147@163.com>
2019-09-18 14:40:14 +0800

    test-brance

---
+++ git-mybatis-upms/git-mybatis-upms-biz/src/main/java/com/kylin/upms/biz/config/security/TestBranch.java
@@ -0,0 +1,12 @@
+package com.kylin.upms.biz.config.security;
+
+/**
+ * @Author: zhebin
+ * @Date: 2019/9/18 14:39
+ */
+public class TestBranch {
+
+    public static void main(String[] args) {
+        System.out.println("这是测试分支的功能");
+    }
+}
