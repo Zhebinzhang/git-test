413f2f59b5a43ce9601dd84e281e10912be24f77
zhangzhebin <19913143147@163.com>
2019-09-19 16:56:33 +0800

    test

---
+++ git-mybatis-upms/git-mybatis-upms-biz/src/main/java/com/kylin/upms/biz/config/security/TestMasterDev.java
@@ -0,0 +1,11 @@
+package com.kylin.upms.biz.config.security;
+
+/**
+ * @Author: zhebin
+ * @Date: 2019/9/19 16:55
+ */
+public class TestMasterDev {
+    public static void main(String[] args) {
+        System.out.println("test master dev ");
+    }
+}
